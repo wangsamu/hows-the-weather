cf555d84a59de9a558d6b4a208af6e59b8e82f3c
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-02-07 17:50:53 +0100

    Initial fetch form OpenWeather API added, using useEffect-

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,11 +1,9 @@
 import { View, Text } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
 
 const openWeatherAPIKey = '';
 const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${capitalLatitude}&lon=${capitalLongitude}&appid=${process.env.REACT_APP_API_KEY}`;
-
-//        `https://api.openweathermap.org/data/2.5/weather?lat=${capitalLatitude}&lon=${capitalLongitude}&appid=${process.env.REACT_APP_API_KEY}`
 
 const Weather = () => {
   const [forecast, setForecast] = useState(null);
@@ -15,7 +13,26 @@ const Weather = () => {
     setRefreshing(true);
     const { status } = await Location.requestForegroundPermissionsAsync();
     console.log(status);
+    if (status !== granted) {
+      Alert.alert('Permission to location data needed!');
+    }
+
+    const currentLocation = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+
+    const response = await fetch(openWeatherUrl);
+    const data = response.json();
+
+    if (!reponse.ok) {
+      Alert.alert('Something wet wrong!');
+    }
+
+    setRefreshing(false);
   };
+
+  useEffect(() => loadForecast(), []);
+
   return (
     <View>
       <Text>Weather</Text>
